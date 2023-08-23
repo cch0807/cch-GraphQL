74c70d1c67357900a1405e29bdacbb2944292345
cch0807 <ckdgus11021@gmail.com>
2023-08-23 16:22:57 +0900

    feat: 유니언과 인터페이스 추가

--- index.js
+++ index.js
@@ -4,18 +4,33 @@ const _ = require("lodash");
 const queries = require("./typedefs-resolvers/_queries");
 const mutations = require("./typedefs-resolvers/_mutations");
 const enums = require("./typedefs-resolvers/_enums");
+const people = require("./typedefs-resolvers/people");
 const equipments = require("./typedefs-resolvers/equipments");
+const softwares = require("./typedefs-resolvers/softwares");
 const supplies = require("./typedefs-resolvers/supplies");
+const givens = require("./typedefs-resolvers/givens");
+const tools = require("./typedefs-resolvers/tools");
 
 const typeDefs = [
     queries,
     mutations,
     enums,
+    people.typeDefs,
     equipments.typeDefs,
+    softwares.typeDefs,
     supplies.typeDefs,
+    givens.typeDefs,
+    tools.typeDefs,
 ];
 
-const resolvers = [equipments.resolvers, supplies.resolvers];
+const resolvers = [
+    people.resolvers,
+    equipments.resolvers,
+    supplies.resolvers,
+    softwares.resolvers,
+    givens.resolvers,
+    tools.resolvers,
+];
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
